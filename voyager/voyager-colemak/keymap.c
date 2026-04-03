0a724b497ce32488040d402b00d20d52af74551f
Tristan Bietsch <tb.co@icloud.com>
2026-04-02 17:47:08 -0700

    updated kb

--- voyager/voyager-colemak/keymap.c
+++ voyager/voyager-colemak/keymap.c
@@ -24,14 +24,15 @@ enum tap_dance_codes {
   DANCE_3,
 };
 
+#define DUAL_FUNC_0 LT(6, KC_F7)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_voyager(
     KC_TRANSPARENT, KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_TRANSPARENT, 
     CW_TOGG,        KC_Q,           KC_W,           KC_F,           KC_P,           KC_B,                                           KC_J,           KC_L,           KC_U,           KC_Y,           KC_MINUS,       KC_EQUAL,       
-    MT(MOD_LSFT, KC_ESCAPE),KC_A,           KC_R,           MT(MOD_LALT, KC_S),MT(MOD_LGUI, KC_T),KC_G,                                           KC_M,           MT(MOD_RGUI, KC_N),MT(MOD_LALT, KC_E),KC_I,           KC_O,           TD(DANCE_0),    
+    MT(MOD_LSFT, KC_ESCAPE),KC_A,           MT(MOD_LCTL, KC_R),MT(MOD_LALT, KC_S),MT(MOD_LGUI, KC_T),KC_G,                                           KC_M,           MT(MOD_RGUI, KC_N),MT(MOD_LALT, KC_E),MT(MOD_RCTL, KC_I),KC_O,           TD(DANCE_0),    
     KC_TILD,        KC_Z,           KC_X,           KC_C,           KC_D,           KC_V,                                           KC_K,           KC_H,           KC_COMMA,       KC_DOT,         KC_SLASH,       TD(DANCE_1),    
-                                                    LT(3, KC_SPACE),LT(1, KC_TAB),                                  LT(2, KC_TAB),  KC_BSPC
+                                                    LT(1, KC_SPACE),DUAL_FUNC_0,                                    LT(2, KC_TAB),  LT(3, KC_BSPC)
   ),
   [1] = LAYOUT_voyager(
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
@@ -274,6 +275,21 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
     break;
 
+    case DUAL_FUNC_0:
+      if (record->tap.count > 0) {
+        if (record->event.pressed) {
+          register_code16(KC_ENTER);
+        } else {
+          unregister_code16(KC_ENTER);
+        }
+      } else {
+        if (record->event.pressed) {
+          register_code16(KC_F19);
+        } else {
+          unregister_code16(KC_F19);
+        }  
+      }  
+      return false;
     case RGB_SLD:
       if (record->event.pressed) {
         rgblight_mode(1);
